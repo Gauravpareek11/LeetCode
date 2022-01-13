ecdcdbff88397da51fa0a27938b835749d2f8e59
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-01-13 15:05:00 +0530

    Time: 2625 ms (5.01%), Space: 46.2 MB (45.30%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.java
@@ -0,0 +1,73 @@
+class pair{
+    String v;
+    int dis;
+    pair(String v,int dis){
+        this.v=v;
+        this.dis=dis;
+    }
+}
+class Solution {
+    int ans=Integer.MAX_VALUE;
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if(!wordList.contains(endWord)) return 0;
+        HashMap<String,List<String>>m1=new HashMap<>();
+        List<String>l1=new ArrayList<>();
+        for(String x:wordList){
+            if(wordsMatch(beginWord,x)) l1.add(x);
+        }
+        m1.put(beginWord,l1);
+        for(int i=0;i<wordList.size();i++){
+            List<String>temp=new ArrayList<>();
+            for(String x:wordList){
+                if(wordsMatch(wordList.get(i),x)) temp.add(x);
+            }
+            m1.put(wordList.get(i),temp);
+        }
+        // System.out.println(m1);
+        List<String>Visited=new ArrayList<>();
+        HashMap<String,Integer>dis=new HashMap<>();
+        for(int i=0;i<wordList.size();i++){
+            dis.put(wordList.get(i),Integer.MAX_VALUE);
+        }
+        PriorityQueue<pair>pq=new PriorityQueue<>((a,b)->Integer.compare(a.dis,b.dis));
+        dis.put(beginWord,0);
+        pq.add(new pair(beginWord,1));
+        while(!pq.isEmpty()){
+            pair p=pq.poll();
+            for(String i:m1.get(p.v)){
+                if(dis.get(i)>1+p.dis){
+                    dis.put(i,1+p.dis);
+                    pq.add(new pair(i,dis.get(i)));
+                }
+            }
+        }
+        return (dis.get(endWord)==Integer.MAX_VALUE?0:dis.get(endWord));
+    }
+    boolean wordsMatch(String a,String b){
+        int c=0;
+        if(a.length()!=b.length()) return false;
+        for(int i=0;i<a.length();i++){
+            if(a.charAt(i)!=b.charAt(i)) c++;
+        }
+        return c==1;
+    }
+    void dfs(String a,HashMap<String,List<String>>m1,String endWord,int count,List<String>Visited){
+        Visited.add(a);
+        if(a.equals(endWord)){
+            Visited.remove(a);
+            ans=Math.min(ans,count+1);
+            // System.out.println(ans);
+            // return;
+        }
+        
+        // if(a.equals(endWord)) Visited.remove(a);
+        for(String x:m1.get(a)){
+            if(!Visited.contains(x)){
+                // Visited.add(x);
+                dfs(x,m1,endWord,count+1,Visited);
+                // Visited.remove(x);
+            }
+        }
+        // Visited.remove(a);
+    }
+}
