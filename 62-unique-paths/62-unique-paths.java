ca038a4a2fe556bead3a7226efe435bd59832e1c
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-02-13 20:54:59 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (16.60%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -24,8 +24,17 @@ class Solution {
         for(int i=0;i<m;i++){
             Arrays.fill(dp[i],-1);
         }
-        return solve(m-1,n-1,dp);
-        
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                int up=0,left=0;
+                if(i-1>=0) up=dp[i-1][j];
+                if(j-1>=0) left=dp[i][j-1];
+                dp[i][j]=up+left;
+            }
+        }
+        return dp[m-1][n-1];
     }
     // void solve(int i,int j,int m,int n){
     //     if(i==m-1 && j==n-1){
@@ -46,10 +55,12 @@ class Solution {
     //     }
     //     return true;
     // }
-    int solve(int i,int j,int[][] dp){
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=solve(i-1,j,dp)+solve(i,j-1,dp);
-    }
+    
+    
+    // int solve(int i,int j,int[][] dp){
+    //     if(i==0 && j==0) return 1;
+    //     if(i<0 || j<0) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     return dp[i][j]=solve(i-1,j,dp)+solve(i,j-1,dp);
+    // }
 }
