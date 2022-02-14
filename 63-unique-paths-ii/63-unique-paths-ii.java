dcab846113ad084c17d615737c6e478fc3ca0881
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-02-14 18:59:26 +0530

    Time: 1 ms (57.09%), Space: 42.6 MB (5.99%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -2,15 +2,25 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] matrix) {
         if(matrix[0][0]==1) return 0;
         int[][] dp=new int[matrix.length][matrix[0].length];
+        // for(int i=0;i<matrix.length;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return solve(dp,matrix,matrix.length-1,matrix[0].length-1);
+        dp[0][0]=1;
         for(int i=0;i<matrix.length;i++){
-            Arrays.fill(dp[i],-1);
+            for(int j=0;j<matrix[0].length;j++){
+                if(i==0 && j==0) continue;
+                if(i-1>=0)dp[i][j]+=dp[i-1][j];
+                if(j-1>=0) dp[i][j]+=dp[i][j-1];
+                if(matrix[i][j]==1) dp[i][j]=0;
+            }
         }
-        return solve(dp,matrix,matrix.length-1,matrix[0].length-1);
+        return dp[dp.length-1][dp[0].length-1];
     }
-    int solve(int[][] dp,int[][] matrix,int i,int j){
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0 || matrix[i][j]==1) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=solve(dp,matrix,i-1,j)+solve(dp,matrix,i,j-1);
-    }
+    // int solve(int[][] dp,int[][] matrix,int i,int j){
+    //     if(i==0 && j==0) return 1;
+    //     if(i<0 || j<0 || matrix[i][j]==1) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     return dp[i][j]=solve(dp,matrix,i-1,j)+solve(dp,matrix,i,j-1);
+    // }
 }
