49568574360b3104c95a40538b2db8c167f01a97
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-02-14 19:22:17 +0530

    Time: 3 ms (57.23%), Space: 46.1 MB (13.49%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,15 +1,27 @@
 class Solution {
     public int minPathSum(int[][] grid) {
         int[][] dp=new int[grid.length][grid[0].length];
+        // for(int i=0;i<grid.length;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return solve(grid,grid.length-1,grid[0].length-1,dp);
+        dp[0][0]=grid[0][0];
+        int x=Integer.MAX_VALUE;
+        int y=Integer.MAX_VALUE;
         for(int i=0;i<grid.length;i++){
-            Arrays.fill(dp[i],-1);
+            for(int j=0;j<grid[0].length;j++){
+                if(i==0 && j==0) continue;
+                if(i-1>=0) x=dp[i-1][j];
+                if(j-1>=0) y=dp[i][j-1];
+                dp[i][j]=grid[i][j]+Math.min(x,y);
+            }
         }
-        return solve(grid,grid.length-1,grid[0].length-1,dp);
+        return dp[dp.length-1][dp[0].length-1];
     }
-    int solve(int[][] grid,int i,int j,int[][] dp){
-        if(i==0 && j==0) return grid[i][j];
-        if(i<0 || j<0) return 100000;            //101 is according to the constraints
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=Math.min(grid[i][j]+solve(grid,i-1,j,dp),grid[i][j]+solve(grid,i,j-1,dp));
-    }
+    // int solve(int[][] grid,int i,int j,int[][] dp){
+    //     if(i==0 && j==0) return grid[i][j];
+    //     if(i<0 || j<0) return 100000;            
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     return dp[i][j]=Math.min(grid[i][j]+solve(grid,i-1,j,dp),grid[i][j]+solve(grid,i,j-1,dp));
+    // }
 }
