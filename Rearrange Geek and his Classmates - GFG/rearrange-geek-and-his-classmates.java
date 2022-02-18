b95b5e622304b6ca7acf06e756e91286697100ff
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-02-18 16:58:36 +0530

    Added solution - LeetHub

---
+++ Rearrange Geek and his Classmates - GFG/rearrange-geek-and-his-classmates.java
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            int n = Integer.parseInt(read.readLine());
+            
+            String input_line[] = read.readLine().trim().split("\\s+");
+            long a[]= new long[n];
+            for(int i = 0; i < n; i++)
+                a[i] = Long.parseLong(input_line[i]);
+
+            Solution ob = new Solution();
+            ob.prank(a, n); 
+
+            for (int i=0;i<n;i++) 
+                System.out.print(a[i]+" "); 
+            System.out.println();
+        }
+    } 
+} // } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    void prank(long[] a, int n)  
+    { 
+        // long[] ans=new long[n];
+        HashMap<Long,Long>m1=new HashMap<>();
+        for(int i=0;i<n;i++){
+            m1.put((long)i,a[i]);
+        }
+        for(int i=0;i<n;i++){
+            a[i]=m1.get(a[i]);
+        }
+    }
+} 
