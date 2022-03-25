95d9f86753d630ae2b35567617b9522f12ae86a7
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-03-25 11:46:59 +0530

    Added solution - LeetHub

---
+++ Smallest greater elements in whole array - GFG/smallest-greater-elements-in-whole-array.java
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    int sizeOfArray = Integer.parseInt(br.readLine());
+		    int arr [] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Complete obj = new Complete();
+		    arr = obj.greaterElement(arr, sizeOfArray);
+		    for(int i=0; i< sizeOfArray;i++){
+		        if(arr[i] == -10000000)
+		            System.out.print("_ ");
+		        else
+		            System.out.print(arr[i] + " ");
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//User function Template for Java
+
+//User function Template for Java
+
+            
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int[] greaterElement (int arr[], int n) {
+        // Complete the function
+        TreeSet<Integer>s1=new TreeSet<>();
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++){
+            s1.add(arr[i]);
+        }
+        for(int i=0;i<n;i++){
+            if(s1.higher(arr[i])==null){
+                ans[i]=-(int)1e7;
+            }
+            else ans[i]=s1.higher(arr[i]);
+        }
+        return ans;
+    }
+    
+    
+}
+
+
