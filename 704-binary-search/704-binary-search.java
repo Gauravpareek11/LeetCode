3342b301b30c7e761ac7f78b9592a5556f03466b
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-03-26 14:13:31 +0530

    Time: 0 ms (100.00%), Space: 54.7 MB (21.35%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -3,15 +3,15 @@ class Solution {
         int low=0;
         int high=nums.length-1;
         while(low<=high){
-            int mid=(low+high)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]==target){
                 return mid;
             }
             else if(nums[mid]<target){
-                low=low+1;
+                low=mid+1;
             }
             else{
-                high=high-1;
+                high=mid-1;
             }
         }
         return -1;
