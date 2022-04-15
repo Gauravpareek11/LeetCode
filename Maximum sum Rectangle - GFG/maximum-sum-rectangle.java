937f76db335e7fe98ec38686d58c995ec253223f
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-04-15 14:37:12 +0530

    Added solution - LeetHub

---
+++ Maximum sum Rectangle - GFG/maximum-sum-rectangle.java
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N, M, x, y;
+            String S[] = read.readLine().split(" ");
+            N = Integer.parseInt(S[0]);
+            M = Integer.parseInt(S[1]);
+            int a[][] = new int[N][M];
+            for (int i = 0; i < N; i++) {
+                String s[] = read.readLine().split(" ");
+                for (int j = 0; j < M; j++) a[i][j] = Integer.parseInt(s[j]);
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.maximumSumRectangle(N, M, a));
+        }
+    }
+}// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int maximumSumRectangle(int m, int n, int mat[][]) {
+       int preSum[][] = new int[m + 1][n];
+
+        for (int i = 0; i < m; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+            {
+                preSum[i + 1][j] = 
+                   preSum[i][j] + mat[i][j];
+            }
+        }
+
+        int maxSum = -1;
+        int minSum = Integer.MIN_VALUE;
+        int negRow = 0, negCol = 0;
+        int rStart = 0, rEnd = 0, cStart = 0, cEnd = 0;
+        for (int rowStart = 0; 
+             rowStart < m; 
+             rowStart++) 
+        {
+            for (int row = rowStart; row < m; row++) 
+            {
+                int sum = 0;
+                int curColStart = 0;
+                for (int col = 0; col < n; col++) 
+                {
+                    sum += preSum[row + 1][col]
+                           - preSum[rowStart][col];
+                    if (sum < 0) {
+                        if (minSum < sum) {
+                            minSum = sum;
+                            negRow = row;
+                            negCol = col;
+                        }
+                        sum = 0;
+                        curColStart = col + 1;
+                    }
+                    else if (maxSum < sum) 
+                    {
+                        maxSum = sum;
+                        rStart = rowStart;
+                        rEnd = row;
+                        cStart = curColStart;
+                        cEnd = col;
+                    }
+                }
+            }
+        }
+        return maxSum;
+    }
+};
