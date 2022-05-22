0070b5bca409c9ea7ef4df78c8384f611023883a
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-05-22 20:07:38 +0530

    Time: 1712 ms (5.03%), Space: 282 MB (5.19%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,21 +1,27 @@
 class Solution {
     public int countSubstrings(String s) {
         int ans=s.length();
+        HashMap<String,Boolean>m1=new HashMap<>();
         for(int i=0;i<s.length();i++){
             for(int j=i+1;j<s.length();j++){
-                if(check(s.substring(i,j+1))) ans++;
+                if(check(s.substring(i,j+1),m1)) ans++;
             }
         }
         return ans;
     }
-    boolean check(String s){
+    boolean check(String s,HashMap<String,Boolean>m1){
+        if(m1.containsKey(s)) return m1.get(s);
         int a=0;
         int b=s.length()-1;
         while(a<b){
-            if(s.charAt(a)!=s.charAt(b)) return false;
+            if(s.charAt(a)!=s.charAt(b)){
+                m1.put(s,false);
+                return false;
+            }
             a++;
             b--;
         }
+        m1.put(s,true);
         return true;
     }
 }
