844a3a899676a13353ab477cb100289bdff15958
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-05-23 14:12:30 +0530

    Time: 1 ms (91.94%), Space: 44.3 MB (47.29%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,23 +1,25 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp=new int[prices.length+1][2];
+        int[] prev=new int[2];
+        int[] cur=new int[2];
         // for(int a[]:dp){
         //     Arrays.fill(a,-1);
         // }
-        dp[prices.length][0]=dp[prices.length][1]=0;
+        prev[0]=prev[1]=0;
         for(int i=prices.length-1;i>=0;i--){
             for(int buy=0;buy<=1;buy++){
               int profit=0;
               if(buy==1){
-                    profit=Math.max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+                    profit=Math.max(-prices[i]+prev[0],prev[1]);
               }
               else{
-                profit=Math.max(prices[i]+dp[i+1][1],dp[i+1][0]);
+                profit=Math.max(prices[i]+prev[1],prev[0]);
                 }
-                dp[i][buy]=profit;  
+                cur[buy]=profit;  
             }
+            prev=cur;
         }
-        return dp[0][1];
+        return prev[1];
     }
 //     int solve(int i,int buy,int[] prices,int[][] dp){
 //         if(i==prices.length) return 0;
