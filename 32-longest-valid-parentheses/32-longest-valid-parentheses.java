1fe86a767fce6529c6284930a04249128a405ec9
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-05-24 16:23:55 +0530

    Time: 3 ms (74.83%), Space: 42.5 MB (72.17%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -1,15 +1,34 @@
 class Solution {
     public int longestValidParentheses(String s) {
+        // int max=0;
+        // for(int i=0;i<s.length();i++){
+        //     int op=0;
+        //     int closing=0;
+        //     for(int j=i;j<s.length();j++){
+        //         if(s.charAt(j)=='(') op++;
+        //         if(s.charAt(j)==')') closing++;
+        //         if(closing>op) break;
+        //         if(op==closing) max=Math.max(j-i+1,max);
+        //     }
+        // }
+        // return max;
+        
+        
         int max=0;
-        for(int i=0;i<s.length();i++){
-            int op=0;
-            int closing=0;
-            for(int j=i;j<s.length();j++){
-                if(s.charAt(j)=='(') op++;
-                if(s.charAt(j)==')') closing++;
-                if(closing>op) break;
-                if(op==closing) max=Math.max(j-i+1,max);
-            }
+        int op=0;
+        int closing=0;
+        for(int j=0;j<s.length();j++){
+            if(s.charAt(j)=='(') op++;
+            if(s.charAt(j)==')') closing++;
+            if(op==closing) max=Math.max(2*op,max);
+            if(closing>op) closing=op=0;
+        }
+        closing=op=0;
+        for(int j=s.length()-1;j>=0;j--){
+            if(s.charAt(j)=='(') op++;
+            if(s.charAt(j)==')') closing++;
+            if(op==closing) max=Math.max(2*op,max);
+            if(closing<op) closing=op=0;
         }
         return max;
     }
