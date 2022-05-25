cec651aed862619714303e0ba408a5bde66f6975
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-05-25 14:51:56 +0530

    Time: 7 ms (80.32%), Space: 45.3 MB (11.38%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,23 +1,47 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        // int[][] dp=new int[nums.length+1][nums.length+1];
-        int[] cur=new int[nums.length+1];
-        int[] prev=new int[nums.length+1];
-        // for(int[] a:dp){
-        //     Arrays.fill(a,-1);
-        // }
-        // return solve(0,nums,-1,dp);
-        for(int i=nums.length-1;i>=0;i--){
-            for(int x=i-1;x>=-1;x--){
-                int not_pick=prev[x+1];
-                int pick=0;
-                if(x==-1||nums[i]>nums[x]) pick=1+prev[i+1];;
-                cur[x+1]=Math.max(pick,not_pick);
+        ArrayList<Integer>l1=new ArrayList<Integer>();
+        l1.add(nums[0]);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>l1.get(l1.size()-1)) l1.add(nums[i]);
+            else{
+                int ind=search(l1,0,l1.size()-1,nums[i]);
+                l1.set(ind,nums[i]);
             }
-            prev=cur;
         }
-        return prev[0];
+        return l1.size();
     }
+    int search(ArrayList<Integer>l1,int low,int high,int x){
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(l1.get(mid)==x) return mid;
+            else if(l1.get(mid)>x){
+                ans=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return ans;
+    }
+    //     // int[][] dp=new int[nums.length+1][nums.length+1];
+    //     int[] cur=new int[nums.length+1];
+    //     int[] prev=new int[nums.length+1];
+    //     // for(int[] a:dp){
+    //     //     Arrays.fill(a,-1);
+    //     // }
+    //     // return solve(0,nums,-1,dp);
+    //     for(int i=nums.length-1;i>=0;i--){
+    //         for(int x=i-1;x>=-1;x--){
+    //             int not_pick=prev[x+1];
+    //             int pick=0;
+    //             if(x==-1||nums[i]>nums[x]) pick=1+prev[i+1];;
+    //             cur[x+1]=Math.max(pick,not_pick);
+    //         }
+    //         prev=cur;
+    //     }
+    //     return prev[0];
+    // }
     // int solve(int i,int[] nums,int x,int[][] dp){
     //     if(i==nums.length-1){
     //         if(x==-1||nums[i]>nums[x]) return 1;
@@ -29,4 +53,5 @@ class Solution {
     //     if(x==-1||nums[i]>nums[x]) pick=1+solve(i+1,nums,i,dp);
     //     return dp[i][x+1]=Math.max(pick,not_pick);
     // }
+         
 }
