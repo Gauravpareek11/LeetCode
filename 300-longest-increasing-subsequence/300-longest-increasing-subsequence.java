1f5550cbc75380c790b99c1f1ac656a8b5aea655
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-05-25 13:34:41 +0530

    Time: 106 ms (13.93%), Space: 45 MB (16.07%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,19 +1,22 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[][] dp=new int[nums.length+1][nums.length+1];
+        // int[][] dp=new int[nums.length+1][nums.length+1];
+        int[] cur=new int[nums.length+1];
+        int[] prev=new int[nums.length+1];
         // for(int[] a:dp){
         //     Arrays.fill(a,-1);
         // }
         // return solve(0,nums,-1,dp);
         for(int i=nums.length-1;i>=0;i--){
             for(int x=i-1;x>=-1;x--){
-                int not_pick=dp[i+1][x+1];
+                int not_pick=prev[x+1];
                 int pick=0;
-                if(x==-1||nums[i]>nums[x]) pick=1+dp[i+1][i+1];;
-                dp[i][x+1]=Math.max(pick,not_pick);
+                if(x==-1||nums[i]>nums[x]) pick=1+prev[i+1];;
+                cur[x+1]=Math.max(pick,not_pick);
             }
+            prev=cur;
         }
-        return dp[0][0];
+        return prev[0];
     }
     // int solve(int i,int[] nums,int x,int[][] dp){
     //     if(i==nums.length-1){
