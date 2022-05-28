3654e00ef6091f0eea48ac4d362c4c2f600c109b
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-05-28 18:45:52 +0530

    Time: 135 ms (42.27%), Space: 43.1 MB (46.45%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.java
+++ 312-burst-balloons/312-burst-balloons.java
@@ -7,20 +7,31 @@ class Solution {
         }
         l1.add(1);
         int[][] dp=new int[l1.size()][l1.size()];
-        for(int i[]:dp){
-            Arrays.fill(i,-1);
+        // for(int i[]:dp){
+        //     Arrays.fill(i,-1);
+        // }
+        // return solve(1,nums.length,l1,dp);
+        for(int i=nums.length;i>0;i--){
+            for(int j=i;j<=nums.length;j++){
+                int max=0;
+                for(int k=i;k<=j;k++){
+                    int steps=l1.get(i-1)*l1.get(k)*l1.get(j+1)+dp[i][k-1]+dp[k+1][j];
+                    max=Math.max(max,steps);
+                }
+                dp[i][j]=max;
+            }
         }
-        return solve(1,nums.length,l1,dp);
+        return dp[1][nums.length];
     }
-    int solve(int i,int j,ArrayList<Integer>l1,int[][] dp){
-        if(i>j) return 0;
+//     int solve(int i,int j,ArrayList<Integer>l1,int[][] dp){
+//         if(i>j) return 0;
         
-        if(dp[i][j]!=-1) return dp[i][j];
-        int max=0;
-        for(int k=i;k<=j;k++){
-            int steps=l1.get(i-1)*l1.get(k)*l1.get(j+1)+solve(i,k-1,l1,dp)+solve(k+1,j,l1,dp);
-            max=Math.max(max,steps);
-        }
-        return dp[i][j]=max;
-    }
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         int max=0;
+//         for(int k=i;k<=j;k++){
+//             int steps=l1.get(i-1)*l1.get(k)*l1.get(j+1)+solve(i,k-1,l1,dp)+solve(k+1,j,l1,dp);
+//             max=Math.max(max,steps);
+//         }
+//         return dp[i][j]=max;
+//     }
 }
