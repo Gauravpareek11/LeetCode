9bba54d89ac1eb76f226612a6c84794681a4336d
Gauravpareek11 <83057969+Gauravpareek11@users.noreply.github.com>
2022-05-28 20:02:36 +0530

    Time: 1 ms (95.95%), Space: 40.3 MB (97.82%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,20 +1,30 @@
 class Solution {
     public int numDecodings(String s) {
-        int[] dp=new int[s.length()];
-        Arrays.fill(dp,-1);
-        return solve(s,0,dp);
+        int[] dp=new int[s.length()+1];
+        dp[s.length()]=1;
+        // Arrays.fill(dp,-1);
+        // return solve(s,0,dp);
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)=='0')continue;
+            int ans=0;
+
+            ans+=dp[i+1];
+            if(i<s.length()-1 && isValid(s.substring(i,i+2))) ans+=dp[i+2];
+            dp[i]=ans;
+        }
+        return dp[0];
     }
-    int solve(String s,int i,int[] dp){
-        if(i==s.length()) return 1;
+//     int solve(String s,int i,int[] dp){
+//         if(i==s.length()) return 1;
         
-        if(dp[i]!=-1) return dp[i];
-        if(s.charAt(i)=='0') return dp[i]=0;
-        int ans=0;
+//         if(dp[i]!=-1) return dp[i];
+//         if(s.charAt(i)=='0') return dp[i]=0;
+//         int ans=0;
         
-        ans+=solve(s,i+1,dp);
-        if(i<s.length()-1 && isValid(s.substring(i,i+2))) ans+=solve(s,i+2,dp);
-        return dp[i]=ans;
-    }
+//         ans+=solve(s,i+1,dp);
+//         if(i<s.length()-1 && isValid(s.substring(i,i+2))) ans+=solve(s,i+2,dp);
+//         return dp[i]=ans;
+//     }
     boolean isValid(String s){
         if(s.charAt(0)=='0') return false;
         if(Integer.parseInt(s)<=26) return true;
