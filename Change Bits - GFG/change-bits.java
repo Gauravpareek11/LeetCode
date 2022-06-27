fc81e11a5d86694aa001a839150fd3886ec4ef8b
Gaurav Pareek <83057969+Gauravpareek11@users.noreply.github.com>
2022-06-27 19:04:42 +0530

    Added solution - LeetHub

---
+++ Change Bits - GFG/change-bits.java
@@ -0,0 +1,38 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            int[] ans = ob.changeBits(N);
+            System.out.println(ans[0]+ " "+ ans[1]);
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int[] changeBits(int N) {
+        // code here
+        int a=N;
+        int ans=1;
+        while(N>0){
+            // System.out.println(N);
+            ans=ans*2;
+            N=N>>1;
+        }
+        ans--;
+        return new int[]{ans-a,ans};
+    }
+};
