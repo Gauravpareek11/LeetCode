20e44c5c45638e54bcf4bd69bdfbaf1b429cccb7
Gaurav Pareek <83057969+Gauravpareek11@users.noreply.github.com>
2022-08-01 09:50:11 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (65.39%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,66 +1,28 @@
 class Solution {
-    // int count=0;
     public int uniquePaths(int m, int n) {
-        // int[][] a=new int[m][n];
-        // for(int i=0;i<m;i++){ //first column 1;
-        //     a[i][0]=1;
-        // }
-        // for(int j=0;j<n;j++){ //first row 1;
-        //     a[0][j]=1;
-        // }
+        int[][] dp=new int[m][n];
+        for(int[] a:dp){
+            Arrays.fill(a,-1);
+        }
+        // dp[0][0]=1;
         // for(int i=1;i<m;i++){
         //     for(int j=1;j<n;j++){
-        //         a[i][j]=a[i-1][j]+a[i][j-1];
+        //         int a=dp[i][j-1];
+        //         int b=dp[i-1][j];
+        //         dp[i][j]=a+b;
         //     }
         // }
-        // return a[m-1][n-1];
-        
-        
-        // count=0;
-        // boolean[][] visited=new boolean[m][n];
-        // solve(0,0,m,n);
-        // return count;
-        int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        dp[0][0]=1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0) continue;
-                int up=0,left=0;
-                if(i-1>=0) up=dp[i-1][j];
-                if(j-1>=0) left=dp[i][j-1];
-                dp[i][j]=up+left;
-            }
-        }
-        return dp[m-1][n-1];
+        // return dp[m-1][n-1];
+        return solve(m-1,n-1,dp);
     }
-    // void solve(int i,int j,int m,int n){
-    //     if(i==m-1 && j==n-1){
-    //         count++;
-    //         return;
-    //     }
-    //     if(!isValid(i,j,m,n)){
-    //         return;
-    //     }
-    //     // visited[i][j]=true;
-    //     solve(i+1,j,m,n);
-    //     solve(i,j+1,m,n);
-    //     // visited[i][j]=false;
-    // }
-    // boolean isValid(int i,int j,int m,int n){
-    //     if(i<0 || j<0 || i>=m || j>=n){
-    //         return false;
-    //     }
-    //     return true;
-    // }
-    
-    
-    // int solve(int i,int j,int[][] dp){
-    //     if(i==0 && j==0) return 1;
-    //     if(i<0 || j<0) return 0;
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     return dp[i][j]=solve(i-1,j,dp)+solve(i,j-1,dp);
-    // }
+    int solve(int i,int j,int[][] dp){
+        if(i<0 || j<0) return 0;
+        if(i==0 && j==0) return 1;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int a=solve(i-1,j,dp);
+        int b=solve(i,j-1,dp);
+        return dp[i][j]=a+b;
+    }
 }
