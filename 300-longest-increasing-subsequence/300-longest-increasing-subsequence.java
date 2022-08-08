ff76deba9fbdbc249284396a16b2285a62f7d7d3
Gaurav Pareek <83057969+Gauravpareek11@users.noreply.github.com>
2022-08-08 12:55:14 +0530

    Time: 6 ms (86.15%), Space: 44.8 MB (32.42%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,45 +1,30 @@
 class Solution {
-    public int lengthOfLIS(int[] arr) {
-        // ArrayList<Integer>l1=new ArrayList<Integer>();
-        // l1.add(nums[0]);
-        // for(int i=1;i<nums.length;i++){
-        //     if(nums[i]>l1.get(l1.size()-1)) l1.add(nums[i]);
-        //     else{
-        //         int ind=search(l1,0,l1.size()-1,nums[i]);
-        //         l1.set(ind,nums[i]);
-        //     }
-        // }
-        // return l1.size();
-        
-        int[][] dp=new int[arr.length][arr.length+1];
-        for(int[] a:dp){
-            Arrays.fill(a,-1);
+    public int lengthOfLIS(int[] nums) {
+        ArrayList<Integer>l1=new ArrayList<Integer>();
+        l1.add(nums[0]);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>l1.get(l1.size()-1)) l1.add(nums[i]);
+            else{
+                int ind=search(l1,0,l1.size()-1,nums[i]);
+                l1.set(ind,nums[i]);
+            }
         }
-         return lis(arr,0,-1,dp);
-	}
-    static int lis(int[] arr,int i,int prev,int[][] dp){
-        if(i==arr.length) return 0;
-        
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-        int not_pick=lis(arr,i+1,prev,dp);
-        int pick=0;
-        if(prev==-1 || arr[prev]<arr[i]) pick=1+lis(arr,i+1,i,dp);
-        return dp[i][prev+1]=Math.max(not_pick,pick);
+        return l1.size();
     }
-
-    // int search(ArrayList<Integer>l1,int low,int high,int x){
-    //     int ans=0;
-    //     while(low<=high){
-    //         int mid=low+(high-low)/2;
-    //         if(l1.get(mid)==x) return mid;
-    //         else if(l1.get(mid)>x){
-    //             ans=mid;
-    //             high=mid-1;
-    //         }
-    //         else low=mid+1;
-    //     }
-    //     return ans;
-    // }
+        
+    int search(ArrayList<Integer>l1,int low,int high,int x){
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(l1.get(mid)==x) return mid;
+            else if(l1.get(mid)>x){
+                ans=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return ans;
+    }
     //     // int[][] dp=new int[nums.length+1][nums.length+1];
     //     int[] cur=new int[nums.length+1];
     //     int[] prev=new int[nums.length+1];
