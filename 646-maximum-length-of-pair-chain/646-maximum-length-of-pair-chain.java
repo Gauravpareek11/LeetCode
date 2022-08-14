ed914a5b7c8f5a8d1b6bc3890bffccbe3034ccd8
Gaurav Pareek <83057969+Gauravpareek11@users.noreply.github.com>
2022-08-14 21:08:59 +0530

    Time: 14 ms (78.66%), Space: 53.9 MB (53.02%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.java
@@ -1,21 +1,30 @@
 class Solution {
     public int findLongestChain(int[][] pairs) {
         
-        Arrays.sort(pairs,(a,b)->Integer.compare(a[0],b[0]));
-        int[][] dp=new int[pairs.length][pairs.length+1];
-        for(int[] a:dp){
-            Arrays.fill(a,-1);
+        Arrays.sort(pairs,(a,b)->Integer.compare(a[1],b[1]));
+        // int[][] dp=new int[pairs.length][pairs.length+1];
+        // for(int[] a:dp){
+        //     Arrays.fill(a,-1);
+        // }
+        // return solve(0,pairs,-1,dp);
+        int ans=0;
+        int end=-(int)1e9;
+        for(int[] a:pairs){
+            if(end<a[0]){
+                ans++;
+                end=a[1];
+            }
         }
-        return solve(0,pairs,-1,dp);
+        return ans;
     }
-    int solve(int i,int[][] pairs,int prev,int[][] dp){
-        if(i==pairs.length) return 0;
+//     int solve(int i,int[][] pairs,int prev,int[][] dp){
+//         if(i==pairs.length) return 0;
         
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-        int not_pick=solve(i+1,pairs,prev,dp);
-        int pick=0;
-        if(prev==-1||pairs[prev][1]<pairs[i][0]) pick=1+solve(i+1,pairs,i,dp);
+//         if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+//         int not_pick=solve(i+1,pairs,prev,dp);
+//         int pick=0;
+//         if(prev==-1||pairs[prev][1]<pairs[i][0]) pick=1+solve(i+1,pairs,i,dp);
         
-        return dp[i][prev+1]=Math.max(pick,not_pick);
-    }
+//         return dp[i][prev+1]=Math.max(pick,not_pick);
+//     }
 }
