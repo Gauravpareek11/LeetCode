b3d4e7205f8a63ca5e6dc65e167a213e1a7b6c07
Gaurav Pareek <83057969+Gauravpareek11@users.noreply.github.com>
2023-02-06 11:21:35 +0530

    Added solution - LeetHub

---
+++ BST Downward Traversal - GFG/bst-downward-traversal.java
@@ -0,0 +1,148 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        PrintWriter out=new PrintWriter(System.out);
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+    	    	int target=Integer.parseInt(br.readLine());
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+        	    long ans=g.verticallyDownBST(root,target);
+        	    out.println(ans);
+                t--;
+            
+        }
+        out.close();
+    }
+  
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    long verticallyDownBST(Node root,int target)
+    {
+        while(root!=null){
+            if(root.data==target) break;
+            else if(root.data>target){
+                root=root.left;
+            }
+            else root=root.right;
+        }
+        if(root==null) return -1;
+        Queue<pair>q1=new LinkedList<>();
+        q1.add(new pair(root,0));
+        long sum=-root.data;
+        while(!q1.isEmpty()){
+            pair p=q1.poll();
+            if(p.b==0) sum+=p.a.data;
+            if(p.a.left!=null) q1.add(new pair(p.a.left,p.b-1));
+            if(p.a.right!=null) q1.add(new pair(p.a.right,p.b+1));
+        }
+        return sum;
+    }
+}
+class pair{
+    Node a;
+    int b;
+    pair(Node a,int b){
+        this.a=a;
+        this.b=b;
+    }
+}
